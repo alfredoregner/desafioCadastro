bb254ab5319b9dac500101626f21dc99849a5203
AlfredoRegner <alfredoregner3@gmail.com>
2024-12-02 21:20:26 -0300

    Criado estrutura base do sistema

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function adicionar(){
+    let exibir = document.querySelector("#adicionarForm");
+    exibir.classList.replace("oculto", "visivel");
+}
+
+function salvar(){
+    let evento = document.querySelector(".evento").ariaValueMax;
+    let ingresso = parseFloat(document.querySelector(".ingresso").value);
+    let dataInicio = document.querySelector("dataInicio");
+    let dataFim = document.querySelector(".dataFim");
+}
+
+function editar(){
+
+}
+
+function excluir(){
+
+}
