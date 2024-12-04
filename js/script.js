3b73bfb00a35d5cf02b80521bca108f74319f537
AlfredoRegner <alfredoregner3@gmail.com>
2024-12-03 21:30:55 -0300

    Finalizado lógica inicial de cadastro de informações na tabela

--- js/script.js
+++ js/script.js
@@ -4,14 +4,41 @@ function adicionar(){
 }
 
 function salvar(){
-    let evento = document.querySelector(".evento").ariaValueMax;
-    let ingresso = parseFloat(document.querySelector(".ingresso").value);
-    let dataInicio = document.querySelector("dataInicio");
-    let dataFim = document.querySelector(".dataFim");
+    //Criando a tabela
+    let tabela = document.querySelector("#eventos");
+    let linha = 0;
+    novaLinha = tabela.insertRow(linha+1);
+    console.log(novaLinha);
+
+    //Coletando as informações dos campos
+
+    let evento = novaLinha.insertCell(0); //Cria a coluna
+    evento.innerHTML = document.querySelector(".evento").value; //Insere o valor
+
+    let ingresso = novaLinha.insertCell(1); //cria a coluna
+    ingresso.innerHTML = parseFloat(document.querySelector(".ingresso").value);
+    
+    let dataInicio = novaLinha.insertCell(2);
+    dataInicio.innerHTML = document.querySelector(".dataInicio").value;
+    
+    let dataFim = novaLinha.insertCell(3);
+    dataFim.innerHTML = document.querySelector(".dataFim").value;
+
+    // let botaoEditar = novaLinha.insertCell(4);
+    // botaoEditar = document.createElement("button");
+    // botaoEditar.setAttribute("id", "editar");
+    // botaoEditar.setAttribute("onclick", "editar()");
+    // botaoEditar.innerHTML = "Editar"
+
+    // let botaoExcluir = novaLinha.insertCell(4);
+    // botaoExcluir = document.createElement("button");
+    // botaoExcluir.setAttribute("id", "excluir");
+    // botaoExcluir.setAttribute("onclick", "excluir()");
+    // botaoExcluir.innerHTML = "Excluir"
 }
 
 function editar(){
-
+        
 }
 
 function excluir(){
